b56622640006379be57dfdaec02c4603eac55034
Olexandr Darmoroz <darmoroz99@gmail.com>
2024-07-10 15:23:46 +0300

    add categories strucutre

--- src/turbinsAndTurbosklad/main.js
+++ src/turbinsAndTurbosklad/main.js
@@ -9,14 +9,25 @@ const brand = 'turboSklad';
 const CAT_KEYS = {
   Актуатор: 'Клапан(актуатор) турбіни',
   Геометрия: 'Сопловий апарат(геометрія) турбіни',
-  Картирдж: 'Картридж турбіни',
+  Сопловий: 'Сопловий апарат(геометрія) турбіни',
+  Картридж: 'Картридж турбіни',
   Корпус: 'Корпус турбокомпресора',
   Сервопривід: 'Електронний актуатор турбіни(сервопривід)',
   Турбина: 'Турбокомпресор',
+  Турбіна: 'Турбокомпресор',
   'novye-turbiny': 'Турбокомпресор',
   'kartridzhi-srednyaya': 'Картридж турбіни',
   'vakuumnye-aktuatory': 'Клапан(актуатор) турбіни',
   'geometriya-turbiny': 'Сопловий апарат(геометрія) турбіни',
+};
+
+const CAT_KEYS_2 = {
+  Турбокомпресор: 'Нові турбіни',
+  'Картридж турбіни': 'Картриджі',
+  'Сопловий апарат(геометрія) турбіни': 'Геометрії',
+  'Клапан(актуатор) турбіни': 'Актуатори',
+  'Електронний актуатор турбіни(сервопривід)': 'Електронні актуатори',
+  'Корпус турбокомпресора': 'Корпус турбіни',
 };
 
 async function filterData(filePathJson, brand) {
@@ -143,25 +154,32 @@ async function filterData(filePathJson, brand) {
 // concatDataJsonFiles('applicab', 'applicab')
 async function fix(filePath) {
   const items = await parseJSONFile(filePath);
+
   items.forEach(it => {
-    const nameRu = it.name_uk
-      .replace(/турбіни/, 'турбины')
-      .replace(/електронний/, 'электронный')
-      .replace('Сопловий апарат (геометрія)', 'Сопловой аппарат (геометрия)')
-      .replace('Електронний', 'Электронный')
-      .replace('сервопривід', 'сервопривод')
-      .replace('Турбіна', 'Турбина')
-      .replace('Турбокомпресор', 'Турбокомпрессор')
-      .replace('керування', 'управления')
-      .replace('турбіною', 'турбиной')
-      .replace('Геометрія', 'Геометрия');
-    it.name_ru = nameRu;
-    it.meta_title_ru=nameRu;
-    it.meta_h1_ru = null;
-    it.meta_description_ru=`Купить ${nameRu}`;
-    const desc_ru=it.description_uk.replace('КОДИ', 'КОДЫ').replace('КРОС-КОДИ','КРОСС-КОДЫ').replace('Застосовність','Применимость').replace('Застосовується в турбінах','Применяется в турбинах').replace('АВТОМОБІЛЬ','АВТОМОБИЛЬ').replace('ОРИГІНАЛЬНИЙ НОМЕР ТУРБІНИ','ОРИГИНАЛЬНЫЙ НОМЕР ТУРБИНЫ').replace('ЗАВОДСЬКИЙ НОМЕР ТУРБІНИ','ЗАВОДСКОЙ НОМЕР ТУРБИНЫ').replaceAll('турбіни', 'турбины')
-    it.description_ru = desc_ru;
+    const cat=it.product_category
+    const brand=it.manufacturer
+    it.product_category=`${it.product_category}|${it.product_category}>${CAT_KEYS_2[cat]} ${brand}`
   });
+
+  // items.forEach(it => {
+  //   const nameRu = it.name_uk
+  //     .replace(/турбіни/, 'турбины')
+  //     .replace(/електронний/, 'электронный')
+  //     .replace('Сопловий апарат (геометрія)', 'Сопловой аппарат (геометрия)')
+  //     .replace('Електронний', 'Электронный')
+  //     .replace('сервопривід', 'сервопривод')
+  //     .replace('Турбіна', 'Турбина')
+  //     .replace('Турбокомпресор', 'Турбокомпрессор')
+  //     .replace('керування', 'управления')
+  //     .replace('турбіною', 'турбиной')
+  //     .replace('Геометрія', 'Геометрия');
+  //   it.name_ru = nameRu;
+  //   it.meta_title_ru=nameRu;
+  //   it.meta_h1_ru = null;
+  //   it.meta_description_ru=`Купить ${nameRu}`;
+  //   const desc_ru=it.description_uk.replace('КОДИ', 'КОДЫ').replace('КРОС-КОДИ','КРОСС-КОДЫ').replace('Застосовність','Применимость').replace('Застосовується в турбінах','Применяется в турбинах').replace('АВТОМОБІЛЬ','АВТОМОБИЛЬ').replace('ОРИГІНАЛЬНИЙ НОМЕР ТУРБІНИ','ОРИГИНАЛЬНЫЙ НОМЕР ТУРБИНЫ').replace('ЗАВОДСЬКИЙ НОМЕР ТУРБІНИ','ЗАВОДСКОЙ НОМЕР ТУРБИНЫ').replaceAll('турбіни', 'турбины')
+  //   it.description_ru = desc_ru;
+  // });
 
   // items.forEach((item, idx) => {
   //   const { sku, applicability } = item;
@@ -178,5 +196,5 @@ async function fix(filePath) {
 
   await saveToJson('', filePath, items);
 }
-// const file = 'common';
-// fix(file);
+const file = 'common';
+fix(file);
