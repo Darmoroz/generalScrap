1612a67521a7f4ead5f6c08d387e6f1dc36ee806
Alex <darmoroz99@gmail.com>
2024-10-03 18:37:30 +0300

    primeturbo Analise

--- src/index.js
+++ src/index.js
@@ -3,29 +3,31 @@ import puppeteer from 'puppeteer';
 import chalk from 'chalk';
 import fs from 'fs';
 import { JSDOM } from 'jsdom';
+import { parseJSONFile } from './commonUtils/parseJSONFile.js';
+import { saveToJson } from './commonUtils/saveToJson.js';
 
-const savePath = `../data/`;
-const saveFileName = '.json'; //!
-
-async function main(params) {
+async function main() {
   try {
+    const items = await parseJSONFile('mastershopFull');
+    console.log(items.length);
+    const uniqByLink = getUniqObjByKey(items, 'links');
+    console.log(uniqByLink.length);
+    await saveToJson('', 'mastershopFull', uniqByLink);
   } catch (error) {
     console.log(chalk.red(error));
   }
 }
 
-main();
+await main();
 
-// (async function () {
-//   for (let i = 0; i < URLS_PATH.length; i++) {
-//     const category = URLS_PATH[i];
-//     try {
-//       const { data } = await axios.get(``); //!
-//       const { document } = new JSDOM(data).window;
-//       const products = [...document.querySelector('').querySelectorAll('')]; //!
-//     } catch (error) {
-//       console.log(chalk.red(error));
-//     }
-//   }
-//   // saveDataCSV(convertToCSV(result.flat()), outputFileName);
-// })();
+function getUniqObjByKey(array, key) {
+  const seen = new Set();
+  return array.filter(item => {
+    const keyValue = item[key];
+    if (!seen.has(keyValue)) {
+      seen.add(keyValue);
+      return true;
+    }
+    return false;
+  });
+}
